5b3efe8776340b10e6ea93001ffa907fd7b67a6c
Anil <anilfth2001@gmail.com>
2023-12-08 09:51:51 +0530

    realproject commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Components/Header/Header'
+import Hero from './Components/Hero/Hero'
+import './App.css'
+import Companies from './Companies/Companies'
+function App() {
+  return (
+    <div className='App'>
+      <div> 
+      <div className="white-gradient"/>
+       <Header />
+       <Hero/>
+       </div>
+       <Companies/>
+    </div>
+  )
+}
+
+export default App
